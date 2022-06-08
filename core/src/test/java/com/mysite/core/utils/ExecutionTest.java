595bfc5631d0aed730f82be5ed66641c08e7c154
arpitvarshney <arpitvarshney18@gmail.com>
2022-06-08 12:31:20 +0530

    Updated test cases for ExecutionTest.

--- core/src/test/java/com/mysite/core/utils/ExecutionTest.java
+++ core/src/test/java/com/mysite/core/utils/ExecutionTest.java
@@ -3,35 +3,34 @@ package com.mysite.core.utils;
 import org.junit.*;
 
 public class ExecutionTest {
+
     @BeforeClass
-    public static void before_class()
-    {
+    public static void before_class() {
         System.out.println("This method is executed before ExecutionTest class");
     }
 
     @Before
-    public void before(){
+    public void before() {
         System.out.println("This method is executed before every test method");
     }
 
     @After
-    public void after(){
+    public void after() {
         System.out.println("This method is executed after every test method");
     }
 
     @AfterClass
-    public static void after_class(){
+    public static void after_class() {
         System.out.println("This method is executed before ExecutionTest class");
     }
 
     @Test
-    public void testcase1()
-    {
+    public void testcase1() {
         System.out.println("Test case1");
     }
+
     @Test
-    public void testcase2()
-    {
+    public void testcase2() {
         System.out.println("Test case2");
     }
 }
