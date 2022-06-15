83f24cd39823c030fefc5a745b31b5a2d8ffcda8
arpitvarshney <arpitvarshney18@gmail.com>
2022-06-15 17:15:25 +0530

    Updated Test Case for SampleModel.

--- core/src/test/java/com/mysite/core/models/SampleModelTest.java
+++ core/src/test/java/com/mysite/core/models/SampleModelTest.java
@@ -1,26 +1,18 @@
 package com.mysite.core.models;
 
 import com.mysite.core.utils.JcrUtils;
-import org.apache.commons.lang.reflect.FieldUtils;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ValueMap;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.MockitoRule;
+import org.mockito.*;
+import org.mockito.junit.*;
 import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ JcrUtils.class })
@@ -35,12 +27,9 @@ public class SampleModelTest {
     @Mock
     private Resource resource;
 
-    @Mock
-    private ValueMap valueMap;
-
     @Before
-    public void before() throws IllegalAccessException {
-        FieldUtils.writeField(sampleModel, "propertyName", "productPagePath", true);
+    public void before() {
+        sampleModel.propertyName = "productPagePath";
         PowerMockito.mockStatic(JcrUtils.class);
         PowerMockito.when(JcrUtils.getPageProperty(resource, "productPagePath")).thenReturn("/content/mysite/en/product");
     }
